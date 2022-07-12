8f9ea999490193c2a3cace253415bf34322cae1f
subicura <subicura@subicura.com>
2022-07-12 16:36:25 +0900

    fix meta

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -162,28 +162,28 @@ export default {
     [
       "meta",
       {
-        name: "og:title",
+        property: "og:title",
         content: "코드앤버터 안내서",
       },
     ],
     [
       "meta",
       {
-        name: "og:image",
+        property: "og:image",
         content: "https://docs.codenbutter.com/imgs/share.png",
       },
     ],
     [
       "meta",
       {
-        name: "og:type",
+        property: "og:type",
         content: "website",
       },
     ],
     [
       "meta",
       {
-        name: "og:site_name",
+        property: "og:site_name",
         content: "코드앤버터 안내서",
       },
     ],
